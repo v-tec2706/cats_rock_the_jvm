c5576a834bef3c514ba9858d3473c73b93900698
wsoczek <witus2706@gmail.com>
2021-11-07 12:58:46 +0100

    apps

---
+++ src/main/scala/part1effects/IOApps.scala
@@ -0,0 +1,20 @@
+package part1effects
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+
+import scala.io.StdIn
+
+object IOApps {
+}
+
+object TestApp {
+  val program: IO[Unit] = for {
+    line <- IO(StdIn.readLine())
+    _ <- IO(println(s"Output: $line"))
+  } yield ()
+
+  def main(args: Array[String]): Unit = {
+    program.unsafeRunSync()
+  }
+}
