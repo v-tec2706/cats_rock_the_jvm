9d811f51bbfc7aa3cdc9b08d24ebff0986c778ac
wsoczek <witus2706@gmail.com>
2021-11-07 13:43:51 +0100

    parallelism

---
+++ src/main/scala/part1effects/IOParallelism.scala
@@ -0,0 +1,43 @@
+package part1effects
+
+import cats.Parallel
+import cats.effect.IO.Par
+import cats.effect.implicits.commutativeApplicativeForParallelF
+import cats.effect.{IO, IOApp}
+
+object IOParallelism extends IOApp.Simple {
+
+  // IO are usually sequential
+  import utils._
+  import cats.syntax.apply._
+
+  val foo: IO[String] = IO.delay("foo")
+  val bar: IO[String] = IO.delay("bar")
+  val both: IO[String] = (foo.debug, bar.debug).mapN((foo, bar) => s"both: $foo, $bar")
+
+  // parallelism on IOs
+  // convert sequential IO to parallel IO
+  val parIO1: IO.Par[String] = Parallel[IO].parallel(foo.debug)
+  val parIO2: IO.Par[String] = Parallel[IO].parallel(bar.debug)
+  val bothPar: Par[String] = (parIO1, parIO2).mapN((foo, bar) => s"both: $foo, $bar")
+  // turn back to sequential
+  val bothParV2: IO[String] = Parallel[IO].sequential(bothPar)
+
+  // shorthand:
+  import cats.syntax.parallel._
+  val bothParV3: IO[String] = (foo.debug, bar.debug).parMapN((foo, bar) => s"both: $foo, $bar")
+
+  // failure
+  val aFail: IO[String] = IO.raiseError(new RuntimeException("Error here!"))
+  // compose success and failure
+  val parallelWithFailure: IO[String] = (foo.debug, aFail.debug).parMapN(_ + _)
+
+  // compose two failures
+  val anotherFailure: IO[String] = IO.raiseError(new RuntimeException("Another failure"))
+  val twoFailures: IO[String] = (aFail.debug, anotherFailure.debug).parMapN(_ + _)
+
+  // the first effect to fail gives the final failure
+  val twoFailuresDelayed: IO[String] = (IO(Thread.sleep(1000)) >> aFail.debug, anotherFailure).parMapN(_ + _)
+
+  override def run: IO[Unit] = twoFailuresDelayed.debug.void
+}
---
+++ src/main/scala/utils/package.scala
@@ -0,0 +1,12 @@
+import cats.effect.IO
+
+package object utils {
+  implicit class DebugWrapper[A](io: IO[A]) {
+    def debug: IO[A] = for {
+      a <- io
+      t = Thread.currentThread().getName
+      _ = println(s"[$t] $a")
+    } yield a
+  }
+}
+
